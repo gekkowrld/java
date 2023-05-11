7113b5c79a9a5f9c7b852b02bce141958b7f7add
Gekko Wrld <108436933+gekkowrld@users.noreply.github.com>
2023-05-11 14:45:31 +0300

    chore: Create a hello world program

---
+++ basics/run.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Get the latest .java file in the current directory
+my_file=$(ls -t *.java | head -n1)
+
+# Compile the Java code
+javac $my_file
+
+# Extract the filename (without extension) from the compiled .class files
+my_e=$(ls -t *.class | head -n1)
+my_exec=${my_e%.*}
+# Check if compilation was successful
+if [ $? -eq 0 ]; then
+    echo "Compilation successful"
+
+    # Run the Java program
+    java $my_exec
+
+else
+    echo "Compilation failed"
+fi
