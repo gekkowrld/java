b02d642231abd22fc3b412115f0bdda7496a1ba4
Gekko Wrld <108436933+gekkowrld@users.noreply.github.com>
2023-05-14 12:52:33 +0300

    Remove comments

--- basics/run.sh
+++ basics/run.sh
@@ -1,20 +1,14 @@
 #!/bin/bash
 
-# Clear the console
 clear
 
-# Get the latest .java file in the current directory
 my_file=$(ls -t *.java | head -n1)
 
-# Compile the Java code
 javac $my_file
 
-# Extract the filename (without extension) from the compiled .class files
 my_e=$(ls -t *.class | head -n1)
 my_exec=${my_e%.*}
-# Check if compilation was successful
 if [ $? -eq 0 ]; then
-    # Run the Java program
     java $my_exec
 
 	rm ${my_exec}.class
