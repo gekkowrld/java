83c041ca34577c09e2cd805dd06f1f037bd533ee
Gekko Wrld <108436933+gekkowrld@users.noreply.github.com>
2023-05-15 10:50:29 +0300

    Add a function that counts the lenght of a string

---
+++ basics/Strlen.java
@@ -0,0 +1,13 @@
+public class Strlen{
+	public static void main(String[] args)
+	{
+		String advice;
+		advice = "Seize the day";
+
+		System.out.println(advice.length());
+
+		System.out.print("The number of characters in ");
+		System.out.print("the string \"Hello World!\" is ");
+		System.out.println("Hello World!".length());
+	}
+}
