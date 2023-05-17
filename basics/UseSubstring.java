f2d21fcdabdac23c17ae4b6e28b00343a3b20e1c
Gekko Wrld <108436933+gekkowrld@users.noreply.github.com>
2023-05-17 20:27:43 +0300

    Extract a section of a string using substring

---
+++ basics/UseSubstring.java
@@ -0,0 +1,35 @@
+/**
+ * We'll be using substring method
+ * It can take two arguments:
+ * You must give it a starting point but no need for an ending
+ * If you don't specify the end then the program assumes that it to read to string
+ * For this program we'll be using both parameters but just one can do
+ */
+
+ import java.util.Scanner;
+
+ public class UseSubstring{
+	public static void main(String[] args){
+		Scanner input = new Scanner(System.in);
+
+		String str;
+		int pos1, pos2;
+
+		System.out.println("Enter the string that you want to see a part of");
+		str = input.nextLine();
+		System.out.print("Enter the first position where you want to start seeing your text: ");
+		pos1 = input.nextInt();
+		System.out.print("Enter the last position where you want to see your text: ");
+		pos2 = input.nextInt();
+
+		/**
+		 * For the sake of removing user confusion and making it more "natural"
+		 * We'll be subtracting a one from the number that the user entered
+		 * We do this because java and other programming languages starts counting at zero
+		 */
+
+		System.out.println("The string that we have extracted from position " + pos1 + " to " + pos2 + " of string \"" + str + "\" is " + str.substring(--pos1, --pos2));
+
+		input.close();
+	}
+ }
