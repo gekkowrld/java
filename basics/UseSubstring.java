98d463491cab764dd125ad874111cb4c6dc8645c
Gekko Wrld <108436933+gekkowrld@users.noreply.github.com>
2023-05-17 20:31:42 +0300

    Add extra context

--- basics/UseSubstring.java
+++ basics/UseSubstring.java
@@ -4,6 +4,9 @@
  * You must give it a starting point but no need for an ending
  * If you don't specify the end then the program assumes that it to read to string
  * For this program we'll be using both parameters but just one can do
+ * Let's take that we have two arguments 'a' and 'b'.
+ * The first argument 'a' is calculated by a + 1 while the second argument
+ * 'b' is calculated with b - 1
  */
 
  import java.util.Scanner;
@@ -28,7 +31,7 @@
 		 * We do this because java and other programming languages starts counting at zero
 		 */
 
-		System.out.println("The string that we have extracted from position " + pos1 + " to " + pos2 + " of string \"" + str + "\" is " + str.substring(--pos1, --pos2));
+		System.out.println("The string that we have extracted from position " + pos1 + " to " + pos2 + " of string \"" + str + "\" is " + str.substring(--pos1, pos2));
 
 		input.close();
 	}
