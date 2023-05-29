660cdd13a3d38be68551fcd649f10b2088f7a421
Gekko Wrld <108436933+gekkowrld@users.noreply.github.com>
2023-05-29 18:41:26 +0300

    Add an `if` to make sure that the next step is executed after a succesfull previous one

--- school-work/arrays/run.sh
+++ school-work/arrays/run.sh
@@ -6,12 +6,14 @@ my_file=$(ls -t *.java | head -n1)
 
 javac $my_file
 
-my_e=$(ls -t *.class | head -n1)
-my_exec=${my_e%.*}
-if [ $? -eq 0 ]; then
-    java $my_exec
+if [ $? -eq 0]; then
+	my_e=$(ls -t *.class | head -n1)
+	my_exec=${my_e%.*}
+	if [ $? -eq 0 ]; then
+	    java $my_exec
 
-	rm ${my_exec}.class
-else
-    echo "Compilation failed"
+		rm ${my_exec}.class
+	else
+	    echo "Compilation failed"
+	fi
 fi
